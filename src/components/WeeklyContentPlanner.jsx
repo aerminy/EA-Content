f3984e7375ed2be01a4157c9938af57f5fd9c2fd
aerminy <aquileserminy56@gmail.com>
2025-04-01 18:28:55 -0400

    Update WeeklyContentPlanner.jsx

--- src/components/WeeklyContentPlanner.jsx
+++ src/components/WeeklyContentPlanner.jsx
@@ -10,7 +10,7 @@ const sampleIdeas = [
     day: "Tuesday",
     type: "Skit",
     title: "Trader When Price Hits SL by 0.1 Pip",
-    script: "*violently throws mouse*\\nMe: ‘That was planned.’",
+    script: "*violently throws mouse*\\nMe: That was planned.",
     caption: "Too real 😤 #SLPain",
     youtube: "Skit series: small losses that ruin your day."
   }
